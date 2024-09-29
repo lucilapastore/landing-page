5fd7586b74b851402a8d0aad52a0275abc2f41ed
lucilapastore <lucilapastore@gmail.com>
2024-09-29 20:54:07 -0300

    fix: projects list types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
+                  I&apos;m working on a variety of projects, from simple
                   websites to complex web applications. Here are a few of my
                   favorites.
                 </p>
@@ -133,19 +133,19 @@ export default function Page() {
           <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
             {DATA.projects.map((project, id) => (
               <BlurFade
-                key={project.title}
+                key={project?.title}
                 delay={BLUR_FADE_DELAY * 12 + id * 0.05}
               >
                 <ProjectCard
-                  href={project.href}
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  dates={project.dates}
-                  tags={project.technologies}
-                  image={project.image}
-                  video={project.video}
-                  links={project.links}
+                  href={project?.href}
+                  key={project?.title}
+                  title={project?.title || ""}
+                  description={project?.description || ""}
+                  tags={project?.technologies || []}
+                  dates=""
+                  image={project?.image}
+                  video={project?.video}
+                  links={project?.links}
                 />
               </BlurFade>
             ))}
