685145635bd9cab2010aca842182d7e5b6b94294
lucilapastore <lucilapastore@gmail.com>
2024-11-23 23:49:15 +0700

    fix: update hackathons title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,7 +161,7 @@ export default function Page() {
                   Hackathons
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  I like building things
+                  I like to experiment with web3 technologies
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   During this year, I attended {DATA.hackathons.length}+
